b207655fbf930216ddef863fd4ff70a723ca8642
elvinodelholm <126329471+elvinodelholm@users.noreply.github.com>
2023-06-06 13:52:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,182 @@
+setTimeout(function () {
+    document.getElementById("gif1").style.display = "none";
+    document.getElementById("giftext").style.display = "none";
+    document.getElementById("img1").style.display = "block";
+    document.getElementById("title1").style.display = "block";
+  
+    document.getElementById("img1").addEventListener("click", function () {
+      document.getElementById("gif2").style.display = "block";
+      document.getElementById("giftext").style.display = "block";
+      document.getElementById("img1").style.display = "none";
+      document.getElementById("title1").style.display = "none";
+  
+      setTimeout(function () {
+        document.getElementById("gif2").style.display = "none";
+        document.getElementById("giftext").style.display = "none";
+        document.getElementById("img2").style.display = "block";
+        document.getElementById("title2").style.display = "block";
+      }, 400);
+    });
+  
+    document.getElementById("img2").addEventListener("click", function () {
+      document.getElementById("gif3").style.display = "block";
+      document.getElementById("giftext").style.display = "block";
+      document.getElementById("img2").style.display = "none";
+      document.getElementById("title2").style.display = "none";
+  
+      setTimeout(function () {
+        document.getElementById("gif3").style.display = "none";
+        document.getElementById("giftext").style.display = "none";
+        document.getElementById("img3").style.display = "block";
+        document.getElementById("title3").style.display = "block";
+      }, 400);
+    });
+  
+    document.getElementById("img3").addEventListener("click", function () {
+      document.getElementById("gif4").style.display = "block";
+      document.getElementById("giftext").style.display = "block";
+      document.getElementById("img3").style.display = "none";
+      document.getElementById("title3").style.display = "none";
+  
+      setTimeout(function () {
+        document.getElementById("gif4").style.display = "none";
+        document.getElementById("giftext").style.display = "none";
+        document.getElementById("img4").style.display = "block";
+        document.getElementById("title4").style.display = "block";
+      }, 400);
+    });
+  
+    document.getElementById("img4").addEventListener("click", function () {
+      document.getElementById("gif5").style.display = "block";
+      document.getElementById("giftext").style.display = "block";
+      document.getElementById("img4").style.display = "none";
+      document.getElementById("title4").style.display = "none";
+  
+      setTimeout(function () {
+        document.getElementById("gif5").style.display = "none";
+        document.getElementById("giftext").style.display = "none";
+        document.getElementById("img5").style.display = "block";
+        document.getElementById("title5").style.display = "block";
+      }, 400);
+    });
+  
+    document.getElementById("img5").addEventListener("click", function () {
+      document.getElementById("gif6").style.display = "block";
+      document.getElementById("giftext").style.display = "block";
+      document.getElementById("img5").style.display = "none";
+      document.getElementById("title5").style.display = "none";
+  
+      setTimeout(function () {
+        document.getElementById("gif6").style.display = "none";
+        document.getElementById("giftext").style.display = "none";
+        document.getElementById("img6").style.display = "block";
+        document.getElementById("title6").style.display = "block";
+      }, 400);
+    });
+  
+    document.getElementById("img6").addEventListener("click", function () {
+      document.getElementById("gif7").style.display = "block";
+      document.getElementById("giftext").style.display = "block";
+      document.getElementById("img6").style.display = "none";
+      document.getElementById("title6").style.display = "none";
+  
+      setTimeout(function () {
+        document.getElementById("gif7").style.display = "none";
+        document.getElementById("giftext").style.display = "none";
+        document.getElementById("img7").style.display = "block";
+        document.getElementById("title7").style.display = "block";
+      }, 400);
+    });
+  
+    document.getElementById("img7").addEventListener("click", function () {
+      document.getElementById("gif8").style.display = "block";
+      document.getElementById("giftext").style.display = "block";
+      document.getElementById("img7").style.display = "none";
+      document.getElementById("title7").style.display = "none";
+  
+      setTimeout(function () {
+        document.getElementById("gif8").style.display = "none";
+        document.getElementById("giftext").style.display = "none";
+        document.getElementById("img8").style.display = "block";
+        document.getElementById("title8").style.display = "block";
+      }, 400);
+    });
+  
+    document.getElementById("img8").addEventListener("click", function () {
+      document.getElementById("gif9").style.display = "block";
+      document.getElementById("giftext").style.display = "block";
+      document.getElementById("img8").style.display = "none";
+      document.getElementById("title8").style.display = "none";
+  
+      setTimeout(function () {
+        document.getElementById("gif9").style.display = "none";
+        document.getElementById("giftext").style.display = "none";
+        document.getElementById("img9").style.display = "block";
+        document.getElementById("title9").style.display = "block";
+      }, 400);
+    });
+  
+    document.getElementById("img9").addEventListener("click", function () {
+      document.getElementById("gif10").style.display = "block";
+      document.getElementById("giftext").style.display = "block";
+      document.getElementById("img9").style.display = "none";
+      document.getElementById("title9").style.display = "none";
+  
+      setTimeout(function () {
+        document.getElementById("gif10").style.display = "none";
+        document.getElementById("giftext").style.display = "none";
+        document.getElementById("img10").style.display = "block";
+        document.getElementById("title10").style.display = "block";
+      }, 400);
+    });
+
+    document.getElementById("img10").addEventListener("click", function () {
+      document.getElementById("gif11").style.display = "block";
+      document.getElementById("giftext").style.display = "block";
+      document.getElementById("img10").style.display = "none";
+      document.getElementById("title10").style.display = "none";
+  
+      setTimeout(function () {
+        document.getElementById("gif11").style.display = "none";
+        document.getElementById("giftext").style.display = "none";
+        document.getElementById("img11").style.display = "block";
+        document.getElementById("title11").style.display = "block";
+      }, 400);
+    });
+
+    document.getElementById("img11").addEventListener("click", function () {
+      document.getElementById("gif12").style.display = "block";
+      document.getElementById("giftext").style.display = "block";
+      document.getElementById("img11").style.display = "none";
+      document.getElementById("title11").style.display = "none";
+  
+      setTimeout(function () {
+        document.getElementById("gif12").style.display = "none";
+        document.getElementById("giftext").style.display = "none";
+        document.getElementById("img1").style.display = "block";
+        document.getElementById("title1").style.display = "block";
+      }, 400);
+    });
+
+    document.getElementById("img12").addEventListener("click", function () {
+      document.getElementById("gif13").style.display = "block";
+      document.getElementById("giftext").style.display = "block";
+      document.getElementById("img12").style.display = "none";
+      document.getElementById("title12").style.display = "none";
+  
+      setTimeout(function () {
+        document.getElementById("gif13").style.display = "none";
+        document.getElementById("giftext").style.display = "none";
+        document.getElementById("img1").style.display = "block";
+        document.getElementById("title1").style.display = "block";
+      }, 400);
+    });
+
+  }, 400);
+  
+  function changeText() {
+    var mailText = document.getElementById("mail-text");
+    mailText.innerHTML = "elvin.odelholm@gmail.com";
+  }
+  
+
